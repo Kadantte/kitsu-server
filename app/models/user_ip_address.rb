47e6b7855365277d07147449d33e9cf12a5359a0
Emma Lejeck <nuck@kitsu.io>
2024-11-08 21:20:17 -0800

    Link User IPs to city and ASN

--- app/models/user_ip_address.rb
+++ app/models/user_ip_address.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class UserIpAddress < ApplicationRecord
-  belongs_to :user, required: true
+  belongs_to :user, optional: false
 
   validates :ip_address, presence: true
+
+  def asn
+    IPASN.find_by('start_ip <= ? AND end_ip >= ?', ip_address.to_s, ip_address.to_s)
+  end
+
+  def city
+    IPCity.find_by('start_ip <= ? AND end_ip >= ?', ip_address.to_s, ip_address.to_s)
+  end
 end
