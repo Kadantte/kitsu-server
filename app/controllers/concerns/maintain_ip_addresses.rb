d9cc2b469c4e23be1cd160edd1eed540347c31fc
Emma Lejeck <nuck@kitsu.io>
2024-11-09 20:31:30 -0800

    Maintain UserIpAddress#updated_at
    This allows us to separate “active” from “inactive” IPs

--- app/controllers/concerns/maintain_ip_addresses.rb
+++ app/controllers/concerns/maintain_ip_addresses.rb
@@ -1,8 +1,26 @@
+# frozen_string_literal: true
+
 module MaintainIpAddresses
   extend ActiveSupport::Concern
 
   def maintain_ip_addresses
-    current_user.resource_owner.add_ip(request.remote_ip) if signed_in?
+    return unless signed_in? && current_user.resource_owner
+
+    # RAILS-7: Replace this with UserIpAddress.upsert(update_only:)
+    binds = [
+      [nil, current_user.resource_owner.id],
+      [nil, request.remote_ip],
+      [nil, Time.current]
+    ]
+    UserIpAddress.connection.exec_update(<<~SQL.squish, 'SQL', binds)
+      INSERT INTO user_ip_addresses (user_id, ip_address, created_at, updated_at)
+        VALUES ($1, $2, $3, $3)
+      ON CONFLICT (user_id, ip_address)
+      DO UPDATE SET
+        updated_at = excluded.updated_at,
+        user_id = excluded.user_id,
+        ip_address = excluded.ip_address
+    SQL
   end
 
   included do
