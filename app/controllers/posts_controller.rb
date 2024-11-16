d5ed8339f0a514bd6c0f4ce67c4e320a3e60c243
Emma Lejeck <nuck@kitsu.io>
2024-11-15 21:58:17 -0800

    Limit posting from blocked IPs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,1 +1,13 @@
-class PostsController < ApplicationController; end
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  include CustomControllerHelpers
+
+  def create
+    if IPFilter.by_ip(request.remote_ip).take_action?(:block_posting)
+      render_jsonapi_error(400, 'Your IP address has been blocked from posting')
+    else
+      super
+    end
+  end
+end
