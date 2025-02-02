7533bd96d9ec73d4424402fafb7e1ecd8e09bcdb
Emma Lejeck <nuck@kitsu.io>
2025-02-01 21:08:17 -0800

    Fix HeldReason in graphql api

--- app/graphql/types/enum/held_reason.rb
+++ app/graphql/types/enum/held_reason.rb
@@ -3,6 +3,6 @@
 class Types::Enum::HeldReason < Types::Enum::Base
   description 'The reason a record is held for manual moderator approval.'
 
-  value 'SPAMFILTER', 'Caught by a spam filter.', value: :spamfilter
-  value 'WORDFILTER', 'Caught by a word filter.', value: :wordfilter
+  value 'SPAMFILTER', 'Caught by a spam filter.', value: 'spamfilter'
+  value 'WORDFILTER', 'Caught by a word filter.', value: 'wordfilter'
 end
